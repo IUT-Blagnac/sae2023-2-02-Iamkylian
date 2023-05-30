373f1113751a23ec0aa4889b69019b9172c1af34
Kylian <[kyliangachet@gmail.com]>
2023-05-30 11:05:44 +0200

    ajout phase1

---
+++ Phase1/src/main/EfficaciteMeilleur.java
@@ -0,0 +1,10 @@
+package main;
+
+public class EfficaciteMeilleur {
+
+	public static void main(String[] args) {
+		System.out.println("Bonjour");
+
+	}
+
+}
---
+++ Phase1/src/main/SobrieteMeilleur.java
@@ -0,0 +1,9 @@
+package main;
+
+public class SobrieteMeilleur {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
