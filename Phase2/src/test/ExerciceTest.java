08efbde01ee1cd1bc1d9b7d195f7bac0ae25e761
Kylian <[kyliangachet@gmail.com]>
2023-06-12 19:38:54 +0200

    notation algorithme

--- Phase2/src/test/ExerciceTest.java
+++ Phase2/src/test/ExerciceTest.java
@@ -10,15 +10,15 @@ public class ExerciceTest {
     public void testSolution() {
         // Chaine simple avec ordre complet
         assertEquals(List.of("666","the", "the", "number", "of", "beast"),
-        		main.EfficacitePire.solution("666, the number of the beast",List.of('6', 't', 'n', 'o', 'b')));
+        		main.Exercice.solution("666, the number of the beast",List.of('6', 't', 'n', 'o', 'b')));
         // Chaine à 1 mot
         assertEquals("Erreur de chaine à 1 mot", List.of("OK"),
-        		main.EfficacitePire.solution("OK",List.of('a', 'b', 'c', 'd', 'e')));
+        		main.Exercice.solution("OK",List.of('a', 'b', 'c', 'd', 'e')));
         // Chaine vide
-        assertEquals(List.of(), main.fficacitePire.solution("",List.of('6', 't', 'n', 'o', 'b')));
+        assertEquals(List.of(), main.Exercice.solution("",List.of('6', 't', 'n', 'o', 'b')));
         // Chaine donnée en exemple
         assertEquals(List.of("fait", "Il", "aujourd", "aout", "beau", "hui", "comme", "en"),
-        		main.EfficacitePire.solution("Il fait beau aujourd'hui comme en aout",List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o')));
+        		main.Exercice.solution("Il fait beau aujourd'hui comme en aout",List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o')));
         // Ajoutez vos test ici...
     }
 }
