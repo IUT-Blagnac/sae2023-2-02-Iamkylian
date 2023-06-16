3d289d8aae73c8d88b7dd8d38168e3d669e406cd
Kylian <[kyliangachet@gmail.com]>
2023-06-16 16:55:18 +0200

    efficacité pire finit

--- Phase2/src/exercice/Exercice.java
+++ Phase2/src/exercice/Exercice.java
@@ -1,48 +1,77 @@
 package exercice;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Exercice {
     public static List<String> solution(String str, List<Character> ordre) {
-        Map<Character, Integer> orderMap = new HashMap<>();
-        int index = 0;
-        for (char c : ordre) {
-            orderMap.put(c, index);
-            index++;
+        // Contient le texte splité en mots
+        String[] splited = str.split("[^a-zA-Z0-9]+");
+
+        // Liste qui va contenir le résultat
+        List<String> result = new ArrayList<>();
+
+        // Tableau d’array list de string qui va permettre de stocker les mots dans le bon ordre
+        ArrayList<String>[] tab = new ArrayList[ordre.size()+1];
+        // Initialise les array lists
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = new ArrayList<>();
         }
 
-        List<String> mots = new ArrayList<>();
-        StringBuilder motActuel = new StringBuilder();
-        for (char c : str.toCharArray()) {
-            if (Character.isLetter(c) || Character.isDigit(c)) {
-                motActuel.append(c);
-            } else {
-                if (motActuel.length() > 0) {
-                    mots.add(motActuel.toString());
-                    motActuel.setLength(0);
+        // Liste des mots qui ne commencent pas par un caractère de l’ordre
+        List<String> notfound = new ArrayList<>();
+        for (String s : splited) {
+            // vérifie si la première lettre du mot est dans l’ordre, si oui, on l’ajoute a la liste correspondante
+            // Si il n’y est pas, on l’ajoute a la liste des mots qui ne sont pas ordonné
+            if (ordre.contains(s.charAt(0))){
+                tab[ordre.indexOf(s.charAt(0))].add(s);
+            }else{
+                notfound.add(s);
+            }
+        }
+
+        // Trie les mots dans les listes avec un a bulle
+
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab[i].size(); j++) {
+                for (int k = 0; k < tab[i].size()-j-1; k++) {
+                    if (compare(tab[i].get(k), tab[i].get(k+1), ordre) > 0){
+                        String temp = tab[i].get(k);
+                        tab[i].set(k, tab[i].get(k+1));
+                        tab[i].set(k+1, temp);
+                    }
                 }
             }
         }
-        if (motActuel.length() > 0) {
-            mots.add(motActuel.toString());
+
+
+        // Ajoute le contenu des listes a la liste de sortie
+        for (ArrayList<String> list : tab) {
+            result.addAll(list);
         }
 
-        mots.sort((a, b) -> {
-            char premiereLettreA = Character.toLowerCase(a.charAt(0));
-            char premiereLettreB = Character.toLowerCase(b.charAt(0));
+        // ajoute la liste des mots inconnu a la liste de sortie
+        result.addAll(notfound);
 
-            int orderA = orderMap.getOrDefault(premiereLettreA, Integer.MAX_VALUE);
-            int orderB = orderMap.getOrDefault(premiereLettreB, Integer.MAX_VALUE);
+        // retourne la liste de sortie
+        return result;
+    }
 
-            if (orderA == orderB) {
-                return a.compareToIgnoreCase(b);
+    public static int compare(String mot1, String mot3, List<Character> ordre){
+        int i = 0;
+        while (i < mot1.length() && i < mot3.length()){
+            if (ordre.indexOf(mot1.charAt(i)) < ordre.indexOf(mot3.charAt(i))){
+                return -1;
+            }else if (ordre.indexOf(mot1.charAt(i)) > ordre.indexOf(mot3.charAt(i))){
+                return 1;
             }
-            return Integer.compare(orderA, orderB);
-        });
-
-        return mots;
+            i++;
+        }
+        if (mot1.length() < mot3.length()){
+            return -1;
+        }else if (mot1.length() > mot3.length()){
+            return 1;
+        }
+        return 0;
     }
 }
