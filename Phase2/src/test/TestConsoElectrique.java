965ff1fa9d9d2c1514153c70c77943176d0a27ab
Kylian <[kyliangachet@gmail.com]>
2023-06-16 16:59:03 +0200

    modif testConsoElectrique

--- Phase2/src/test/TestConsoElectrique.java
+++ Phase2/src/test/TestConsoElectrique.java
@@ -12,12 +12,10 @@ public class TestConsoElectrique {
 		List<Character> ordre = Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
 
 		List<String> testStrings = Arrays.asList(
-				"Lorem ipsum dolor sit amet",
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+				"L",
+				"Lorem amet",
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor bibendum turpis, nec pretium odio venenatis sed. Fusce viverra gravida lectus, sed scelerisque lectus iaculis sit amet. In eu mauris id ipsum ultrices ultrices. Morbi rutrum urna et ipsum congue convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris convallis ultricies neque, at cursus metus. Curabitur non diam erat. Maecenas posuere ante id lacinia varius. Suspendisse interdum eleifend nulla, id semper lectus hendrerit quis. Integer sed diam varius, tristique libero ac, fringilla neque. Integer hendrerit rutrum ligula eu tincidunt. Pellentesque nec massa ut dolor facilisis pretium. Nullam iaculis mi a nisi fringilla, ut elementum elit tristique. Duis accumsan neque ac fringilla convallis. In non ante odio. Nullam sed leo consequat, egestas urna id, fringilla massa. Integer lacinia posuere viverra. Proin ut tincidunt felis. Sed interdum feugiat velit ac convallisos."
 				);
 
 		double pcConsumptionPerNs = 5.5555556e-11; // Consommation électrique d'un PC en watt par nano seconde
@@ -25,7 +23,7 @@ public class TestConsoElectrique {
 
 		for (String str : testStrings) {
 			long startTime = System.nanoTime();
-			String result = Exercice.solution(str, ordre);
+			List<String> result = Exercice.solution(str, ordre);
 			long endTime = System.nanoTime();
 			long executionTime = endTime - startTime;
 			double executionTimeHours = executionTime / conversionFactor; // Conversion en heures
@@ -35,7 +33,7 @@ public class TestConsoElectrique {
 			System.out.println("Chaîne de caractères: " + str);
 			System.out.println("Résultat : " + result);
 			System.out.println("Temps d'exécution (en milliseconde): " + tempsExecutionMillis);
-			System.out.printf("Consommation électrique du programme (en Watt-heure): %.5f%n", energyConsumption);
+			System.out.println("Consommation électrique du programme (en Watt-heure): " + energyConsumption);
 			System.out.println("---------------------------------------");
 		}
 
