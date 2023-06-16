744ba158298db8e070f9cc934637a4bf75cd0e04
Kylian <[kyliangachet@gmail.com]>
2023-06-16 22:25:25 +0200

    ajout test supplémentaire

--- Phase2/src/test/ExerciceTest.java
+++ Phase2/src/test/ExerciceTest.java
@@ -6,25 +6,25 @@ import java.util.List;
 import org.junit.Test;
 
 public class ExerciceTest {
-    @Test
-    public void testSolution() {
-        // Chaine simple avec ordre complet
-        assertEquals(List.of("666","the", "the", "number", "of", "beast"),
-        		exercice.Exercice.solution("666, the number of the beast",List.of('6', 't', 'n', 'o', 'b')));
-        // Chaine à 1 mot
-        assertEquals("Erreur de chaine à 1 mot", List.of("OK"),
-        		exercice.Exercice.solution("OK",List.of('a', 'b', 'c', 'd', 'e')));
-        // Chaine vide
-        assertEquals(List.of(), exercice.Exercice.solution("",List.of('6', 't', 'n', 'o', 'b')));
-        // Chaine donnée en exemple
-        assertEquals(List.of("fait", "Il", "aujourd", "aout", "beau", "hui", "comme", "en"),
-        		exercice.Exercice.solution("Il fait beau aujourd'hui comme en aout",List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o')));
-        // Ajoutez vos test ici...
-
-        assertEquals(List.of("fait", "Il", "aujourd", "aout", "beau", "comme", "hui", "en"),
-        		exercice.Exercice.solution("Il fait beau comme aujourd'hui en aout", List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o')));
-
-        assertEquals(List.of("fait", "Il", "aujourd", "aout", "beau", "comme", "hui", "en"),
-        		exercice.Exercice.solution("Il fait beau aout comme aujourd'hui en", List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o')));
-    }
+	@Test
+	public void testSolution() {
+		// Chaine simple avec ordre complet
+		assertEquals(List.of("666","the", "the", "number", "of", "beast"),
+				exercice.Exercice.solution("666, the number of the beast",List.of('6', 't', 'n', 'o', 'b')));
+		// Chaine à 1 mot
+		assertEquals("Erreur de chaine à 1 mot", List.of("OK"),
+				exercice.Exercice.solution("OK",List.of('a', 'b', 'c', 'd', 'e')));
+		// Chaine vide
+		assertEquals(List.of(), exercice.Exercice.solution("",List.of('6', 't', 'n', 'o', 'b')));
+		// Chaine donnée en exemple
+		assertEquals(List.of("fait", "Il", "aujourd", "aout", "beau", "hui", "comme", "en"),
+				exercice.Exercice.solution("Il fait beau aujourd'hui comme en aout",List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o')));
+		
+		// Chaine donnée en exemple
+		assertEquals(List.of("fait", "Il", "aujourd", "aout", "beau", "comme", "hui", "en"),
+				exercice.Exercice.solution("Il fait beau comme aujourd'hui en aout", List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o')));
+		// Chaine donnée avec ordre différent
+		assertEquals(List.of("fait", "Il", "aujourd", "aout", "beau", "comme", "hui", "en"),
+				exercice.Exercice.solution("Il fait beau aout comme aujourd'hui en", List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o')));
+	}
 }
